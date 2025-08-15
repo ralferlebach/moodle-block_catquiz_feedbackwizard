d62860aaf1eb38bcb4ac2e00b406c99e852de6bd
ralferlebach <ralf.erlebach@gmx.de>
2025-08-15 12:19:59 +0200

    Update block_catquiz_feedbackwizard.php

--- lang/en/block_catquiz_feedbackwizard.php
+++ lang/en/block_catquiz_feedbackwizard.php
@@ -1,25 +1,48 @@
-<?php 
-    $string['pluginname'] = 'Multistage form (modal) block';
-    $string['openwizard'] = 'Open multistage wizard'; 
-    $string['privacy:metadata:block_catquiz_feedbackwizard'] = 'Stores multistage form drafts and submissions.'; 
-    $string['privacy:metadata:block_catquiz_feedbackwizard:userid'] = 'The user ID who created the draft/submission.'; 
-    $string['privacy:metadata:block_catquiz_feedbackwizard:courseid'] = 'The course ID where the submission was created.'; 
-    $string['privacy:metadata:block_catquiz_feedbackwizard:datajson'] = 'The partial form data stored as JSON.'; 
-    $string['capability:use'] = 'Use multistage form wizard';
-    $string['step1title'] = 'Step 1: Basic details';
-    $string['step2title'] = 'Step 2: Content'; 
-    $string['step3title'] = 'Step 3: Review & submit'; 
-    $string['field:title'] = 'Title'; 
-    $string['field:category'] = 'Category'; 
-    $string['field:description'] = 'Description'; 
-    $string['field:attachments'] = 'Attachments'; 
-    $string['submitnext'] = 'Continue'; 
-    $string['submitfinal'] = 'Submit'; 
-    $string['savedprogress'] = 'Progress saved.'; 
-    $string['submissionsuccess'] = 'Your submission has been recorded.'; 
-    $string['error:permissiondenied'] = 'You do not have permission to use this wizard.'; 
-    $string['error:invalidstep'] = 'Invalid wizard step.';
-    
-    $string['catquiz_feedbackwizard:addinstance'] = 'Add a new Multistage form block';
-    $string['catquiz_feedbackwizard:myaddinstance'] = 'Add a new Multistage form block to the Dashboard';
-    $string['catquiz_feedbackwizard:use'] = 'Use multistage form wizard';
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Language File for block_catquiz_feedbackwizard (English).
+ *
+ * @package     block_catquiz_feedbackwizard
+ * @copyright   2024 Ralf Erlebach <ralf.erlebach@gmx.de>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['pluginname'] = 'CATQuiz Settings Wizard';
+$string['openwizard'] = 'Start the CATQuiz wizard';
+$string['privacy:metadata:block_catquiz_feedbackwizard'] = 'Stores multistage form drafts and submissions.';
+$string['privacy:metadata:block_catquiz_feedbackwizard:userid'] = 'The user ID who created the draft/submission.';
+$string['privacy:metadata:block_catquiz_feedbackwizard:courseid'] = 'The course ID where the submission was created.';
+$string['privacy:metadata:block_catquiz_feedbackwizard:datajson'] = 'The partial form data stored as JSON.';
+$string['capability:use'] = 'Use multistage form wizard';
+$string['step1title'] = 'Step 1: Basic details';
+$string['step2title'] = 'Step 2: Content';
+$string['step3title'] = 'Step 3: Review & submit';
+$string['field:title'] = 'Title';
+$string['field:category'] = 'Category';
+$string['field:description'] = 'Description';
+$string['field:attachments'] = 'Attachments';
+$string['submitprevious'] = 'Back';
+$string['submitnext'] = 'Continue';
+$string['submitfinal'] = 'Submit';
+$string['savedprogress'] = 'Progress saved.';
+$string['submissionsuccess'] = 'Your submission has been recorded.';
+$string['error:permissiondenied'] = 'You do not have permission to use this wizard.';
+$string['error:invalidstep'] = 'Invalid wizard step.';
+
+$string['catquiz_feedbackwizard:addinstance'] = 'Add a new Multistage form block';
+$string['catquiz_feedbackwizard:myaddinstance'] = 'Add a new Multistage form block to the Dashboard';
+$string['catquiz_feedbackwizard:use'] = 'Use multistage form wizard';
